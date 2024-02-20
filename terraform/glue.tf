ee3e6b18085cedcee8d14af193794e5f19b34e46
hassanalizadeh <hassan.alizadeh@startselect.com>
2024-02-20 15:23:34 +0100

    added a glue job (0014)

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -1,50 +1,44 @@
-data "local_file" "glue_scripts" {
-  depends_on = [local.glue_src_path]
-  for_each   = fileset(local.glue_src_path, "**/*.py")
-
-  filename = "${local.glue_src_path}/${each.key}"
-}
-
-
-locals {
-  glue_jobs = {
-    for filename, _ in data.local_file.glue_scripts : replace(filename, ".py", "") => {
-      name        = replace(filename, ".py", "")
-      script_key  = "glue/scripts/${filename}"
-      script_path = filename
-    }
-  }
-}
-
-resource "aws_s3_object" "glue_job_scripts" {
-  for_each = local.glue_jobs
-
+resource "aws_s3_object" "test_deploy_script_s3" {
   bucket = var.s3_bucket
-  key    = each.value.script_key
-  source = each.value.script_path
-#  etag   = filemd5(each.value.script_path)
+  key = "glue/scripts/TestDeployScript.py"
+  source = "${local.glue_src_path}TestDeployScript.py"
+  etag = filemd5("${local.glue_src_path}TestDeployScript.py")
 }
 
-resource "aws_glue_job" "glue_jobs" {
-  for_each = local.glue_jobs
-
-  name        = each.value.name
-  description = "Test the deployment of an AWS Glue job with Terraform"
-  role_arn    = aws_iam_role.glue_service_role.arn
+resource "aws_glue_job" "test_deploy_script" {
+  /*glue_version = "4.0" #optional*/
+  max_retries = 0 #optional
+  name = "TestDeployScript" #required
+  description = "test the deployment of an aws glue job to aws glue service with terraform" #description
+  role_arn = aws_iam_role.glue_service_role.arn #required
+  /*number_of_workers = 2 #optional, defaults to 5 if not set
+  worker_type = "G.1X" #optional
+  timeout = "60" #optional
+  execution_class = "FLEX" #optional*/
+  tags = {
+    project = var.project #optional
+  }
 
   command {
-    name            = "pythonshell"
-    python_version  = "3.9"
-    script_location = "s3://${var.s3_bucket}/${each.value.script_key}"
+    name= "pythonshell" #"glueetl"  #optional
+    python_version= "3.9"
+    script_location = "s3://${var.s3_bucket}/glue/scripts/TestDeployScript.py" #required
   }
+
 
   default_arguments = {
-    "--additional-python-modules" = "awswrangler"
-    "library-set"                  = "analytics"
-  }
+    "--additional-python-modules": "awswrangler",
+    "library-set": "analytics"    /*
+    "--class"                   = "GlueApp"
+    "--enable-job-insights"     = "true"
+    "--enable-auto-scaling"     = "false"
+    "--enable-glue-datacatalog" = "true"
+    "--job-language"            = "python"
+    "--job-bookmark-option"     = "job-bookmark-disable"
+    "--datalake-formats"        = "iceberg"
 
-  tags = {
-    project = var.project
+    "--conf"                    = "spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions  --conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog  --conf spark.sql.catalog.glue_catalog.warehouse=s3://tnt-erp-sql/ --conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog  --conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO"
+*/
+
   }
 }
-
