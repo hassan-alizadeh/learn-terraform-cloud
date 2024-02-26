da7d6dea72d0e5b5d8d1b6843db425a5dce77462
hassanalizadeh <hassan.alizadeh@startselect.com>
2024-02-26 21:26:37 +0100

    added a glue job (0043)

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -27,3 +27,18 @@ module "glue_job1" {
     job_security_configuration = "my-security-configuration"
     job_notification_email = "*/
 }
+
+module "glue_job2" {
+  source = "../glue/job2/"
+  s3_bucket = var.s3_bucket
+  project = var.project
+  glue_service_role_arn = aws_iam_role.glue_service_role.arn
+#  module_path = var.module_path
+#    job_name = "job1"
+    /*job_role_arn = "arn:aws:iam::123456789012:role/service-role/AWSGlueServiceRole-DefaultRole"
+    job_command_script_location = "s3://my-bucket/glue-scripts/job1.py"
+    job_max_capacity = 2
+    job_timeout = 60
+    job_security_configuration = "my-security-configuration"
+    job_notification_email = "*/
+}
